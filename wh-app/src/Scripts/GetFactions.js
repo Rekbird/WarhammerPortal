454baf1d7e9598571c485a1e2d19205aa4c62adb
Rekbird <43473518+Rekbird@users.noreply.github.com>
2019-03-26 13:31:42 +0300

    Deleted

--- wh-app/src/Scripts/GetFactions.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-import ReturnFactionImages from "../Data/FactionImages/FactionImages.js";
-
-function GetFactions() {
-    var Factions = ReturnFactionImages();
-    return Factions;
-}
-
-export default GetFactions;
