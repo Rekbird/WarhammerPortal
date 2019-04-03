966291645ca31e46d7cf5f1373831bc29b7d62c2
Rekbird <43473518+Rekbird@users.noreply.github.com>
2019-04-03 18:33:29 +0300

    wip

--- warhammer/src/Interface/NewRosterCreation/UnitEditing/UnitEditing.js
+++ warhammer/src/Interface/NewRosterCreation/UnitEditing/UnitEditing.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import "./UnitEditing.css";
 import UnitProfile from "../UnitProfile/UnitProfile.js";
 import UnitModelsList from "../UnitModels/UnitModels.js";
+import WargearElement from "../WargearSelection/WargearElement/WargearElement.js";
 
 class UnitEditing extends Component {
     constructor(props) {
@@ -13,6 +14,7 @@ class UnitEditing extends Component {
             <div>
                 <UnitProfile Unit = {this.props.Unit} UnitSelection = {false}/>
                 <UnitModelsList/>
+                <WargearElement/>
             </div>
         )
     }
