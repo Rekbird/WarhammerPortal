66f890e031f5c29879305ce7909b8dcd05fb7063
Rekbird <43473518+Rekbird@users.noreply.github.com>
2019-05-22 18:25:20 +0300

    Wargear Fininshed

--- warhammer/src/Interface/NewRosterCreation/WargearSelection/WargearSelection.js
+++ warhammer/src/Interface/NewRosterCreation/WargearSelection/WargearSelection.js
@@ -6,28 +6,36 @@ import "./WargearElement/WargearElement.js"
 in:
 RosterModels
 CurrentModel
+UpdateSelectedWargearOptions()
 
 out:
-none
+this.WargearSlots
 */
-
 
 class WargearSelection extends Component {
     constructor(props) {
         super(props);
         this.GetSelectedUnitOptions = this.GetSelectedUnitOptions.bind(this);
-        this.AllSelectedOptions = this.GetSelectedUnitOptions(this.props.RosterModels);
+        this.WargearSlots = this.props.CurrentModel.RosterWargearSlots.slice();
+        this.UpdateSelectedWargearOptions = this.UpdateSelectedWargearOptions.bind(this);
+    }
+
+    UpdateSelectedWargearOptions = (WargearSlots) => {
+        this.props.UpdateSelectedWargearOptions(WargearSlots);
     }
 
     SelectedWargearOption = (SelectedOptionId, CurrentSlot) => {
         CurrentSlot.SelectedOption = CurrentSlot.BaseSlot.Options.filter(option => option.id == SelectedOptionId)[0];
-
+        UpdateSelectedWargearOptions(this.WargearSlots);
     }
 
-    GetAvailableOptions = (CurrentSlot, AllSelectedOptions, CurrentModel) => {
+    GetAvailableOptions = (CurrentSlot, UnitSelectedOptions, CurrentModel) => {
         let AvailableOptions = [];
         let BaseOptions = CurrentSlot.BaseSlot.Options;
         let ModelSelectedOptions = [];
+        let AllSelectedOptions = UnitSelectedOptions;
+        //let AllSelectedOptions = GetSelectedUnitOptions(this.props.RosterModels)
+        let couldBeIncluded;
 
         if (!!CurrentModel.RosterWargearSlots && CurrentModel.RosterWargearSlots.length > 0) {
             for (let i = 0; i < CurrentModel.RosterWargearSlots.length; i++) {
@@ -35,8 +43,26 @@ class WargearSelection extends Component {
             }
         }
         
+        for (let i = 0; i < BaseOptions.length; i++) {
+        let HasLinkedOptions = !!BaseOptions[i].LinkedOptionsId && BaseOptions[i].LinkedOptionsId.length > 0;
+            if (!!BaseOptions[i].PerXmodels) {
+                let AlreadyHave = AllSelectedOptions.filter(option => (option.id == BaseOptions[i].id) || (HasLinkedOptions && BaseOptions[i].LinkedOptionsId.indexOf(option.id) != -1)).length;
+                let CanCarry = this.props.RosterModels.length / BaseOptions[i].PerXmodels;
+                
+                if (CanCarry > AlreadyHave) {
+                    couldBeIncluded = true;
+                }
+            }
 
+            if (!!BaseOptions[i].CountPerModel) {
+                let alreadyHave = ModelSelectedOptions.filter(option => (option.id == BaseOptions[i].id) || (HasLinkedOptions && BaseOptions[i].LinkedOptionsId.indexOf(option.id) != -1)).length;
+                couldBeIncluded = couldBeIncluded && (alreadyHave < BaseOptions[i].CountPerModel);
+            }
 
+            if (couldBeIncluded) {
+                AvailableOptions.push(BaseOptions[i]);
+            }
+        }
         return AvailableOptions;
     }
 
@@ -58,9 +84,11 @@ class WargearSelection extends Component {
     }
  
     render() {
-        const RosterWargearSlots = this.props.RosterWargearSlots.map(
+
+        const UnitSelectedOptions = this.GetSelectedUnitOptions(this.props.RosterModels);
+        const RosterWargearSlots = this.WargearSlots.map(
             (slot) => 
-                <WargearElement key = {slot.id} CurrentSlot = {slot} AvailableOptions = {this.GetAvailableOptions(slot, this.AllSelectedOptions, this.props.CurrentModel)} SelectedWargearOption = {SelectedWargearOption} SelectedOption = {slot.SelectedOption}/>
+                <WargearElement key = {slot.id} CurrentSlot = {slot} AvailableOptions = {this.GetAvailableOptions(slot, UnitSelectedOptions, this.props.CurrentModel)} SelectedWargearOption = {SelectedWargearOption} SelectedOption = {slot.SelectedOption}/>
           );
         return (
             <div>
