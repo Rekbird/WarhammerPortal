5ed66f6157af05e71d6a7e01698adc0b4fd22277
EvgeniyFedeykin <43994833+EvgeniyFedeykin@users.noreply.github.com>
2019-09-12 13:46:36 +0300

    commit;

--- warhammer/src/Interface/ConstructionPlaceholder/ConstructionPlaceholder.js
+++ warhammer/src/Interface/ConstructionPlaceholder/ConstructionPlaceholder.js
@@ -6,9 +6,9 @@ import ConstructionPicture from "../../Data/Pictures/under_construction.jpg";
 
 function ConstructionPlaceholder(props) {
     return (
-        //<div className = "ConstructionPicture" >
+        <div className = "ConstructionPicture" >
             <img src = {ConstructionPicture} className = "ConstructionPicture__Img" />
-        //</div>
+        </div>
     );
           
 }
